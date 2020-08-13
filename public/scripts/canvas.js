1237eeb566ed944617f5450724ac43714475b2ce
Laura Tammpere <laura.tammpere@clio.com>
2020-08-13 12:55:48 -0700

    Added scaffolding for actual game 🤞

---
+++ public/scripts/canvas.js
@@ -0,0 +1,15 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#2d2d2d',
+  scene: {
+    preload: preload
+  }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+  
+}
