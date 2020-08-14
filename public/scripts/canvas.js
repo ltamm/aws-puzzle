52adf70089a6c2bdb1fa33139c27fe219b41404d
Laura Tammpere <laura.tammpere@clio.com>
2020-08-14 12:54:19 -0700

    Did a bit of cleanup

--- public/scripts/canvas.js
+++ public/scripts/canvas.js
@@ -17,32 +17,98 @@ var nat_gateway_enabled = false;
 var ec2, vpc, public_subnet, private_subnet, nat_gateway_toggle
 
 function preload() {
-  this.load.image('vpc', 'assets/vpc.png');
-  this.load.image('pub_subnet', 'assets/pub_subnet.png');
+  this.load.image('vpc',         'assets/vpc.png');
+  this.load.image('pub_subnet',  'assets/pub_subnet.png');
   this.load.image('priv_subnet', 'assets/priv_subnet.png');
-  this.load.image('nat_gw', 'assets/nat.png');
+  this.load.image('nat_gw',      'assets/nat.png');
 
   // Spritesheets
   this.load.spritesheet('ec2', 
-    'assets/ec2.png',
-    { frameWidth: 300, frameHeight: 300 });
+    'assets/ec2.png', {
+      frameWidth: 300,
+      frameHeight: 300
+    }
+  );
 }
+
+
+/*
+ *  Create the world!
+ */
 
 function create() {
 
-  // Create VPC object
-  vpc = this.add.sprite(500, 400, 'vpc');
-  vpc.setScale(.8);
-  vpc.on('drag', on_drag);
-  
-  create_public_subnet(this);
-
-  // Create private subnet object
-  private_subnet = this.add.sprite(610, 475, 'priv_subnet');
-  private_subnet.setScale(.8);
-
-  create_ec2(this);
+  createVPC(this);
+  createEC2(this);
 }
+
+function createVPC(scene) {
+  vpc = scene.add.sprite(500, 400, 'vpc');
+  vpc.setScale(.8);
+  
+  createPublicSubnet(scene);
+
+  private_subnet = scene.add.sprite(610, 475, 'priv_subnet');
+  private_subnet.setScale(.8);
+}
+
+function createPublicSubnet(scene) {
+  bg = scene.add.image(0,0, 'pub_subnet');
+  nat_gateway_toggle = createNATGatewayToggle(scene);
+
+  public_subnet = scene.add.container(610, 325, [bg, nat_gateway_toggle])
+  public_subnet.setSize(bg.width, bg.height);
+  public_subnet.setScale(0.8)
+}
+
+function createNATGatewayToggle(scene) {
+  toggle = scene.add.image(275, 0, 'nat_gw');
+
+  // make toggle interactive
+  // where did 40 come from? Good question! I think it's the height/width of
+  // the toggle image?
+  toggle.setInteractive({
+    hitArea:         new Phaser.Geom.Circle(40, 40, 40),
+    hitAreaCallback: Phaser.Geom.Circle.Contains,
+    useHandCursor:   true
+  });
+
+  toggle.on('pointerup', function() {
+    toggleNatGateway();
+  });
+
+  return toggle;
+}
+
+function createEC2(scene) {
+  ec2 = scene.add.sprite(500, 325, 'ec2');
+  ec2.setScale(0.3);
+
+  // Drag hitbox
+  ec2.setInteractive({
+    hitArea:         new Phaser.Geom.Rectangle(0, 0, 300, 300),
+    hitAreaCallback: Phaser.Geom.Rectangle.Contains,
+    useHandCursor:   true,
+    draggable:       true
+  });
+  ec2.on('drag', on_drag);
+
+  // ec2 animations
+  scene.anims.create({
+    key: 'happy',
+    frames: [ { key: 'ec2', frame: 0} ],
+  });
+
+  scene.anims.create({
+    key: 'sad',
+    frames: [{ key: 'ec2', frame: 1}],
+  });
+}
+
+
+/*
+ *  Update the world!
+ */
 
 function update() {
   if (has_connectivity() ) {
@@ -67,57 +133,16 @@ function has_connectivity() {
   return in_public_subnet || private_subnet_with_gateway;
 }
 
+
+/*
+ *  Helpers
+ */
+
 function on_drag(pointer, dragX, dragY) {
   this.x = dragX;
   this.y = dragY;
 }
 
-function create_ec2(scene) {
-  ec2 = scene.add.sprite(500, 325, 'ec2');
-  ec2.setScale(0.3);
-
-  // Drag hitbox
-  ec2.setInteractive({
-    hitArea: new Phaser.Geom.Rectangle(0, 0, 300, 300),
-    hitAreaCallback: Phaser.Geom.Rectangle.Contains,
-    useHandCursor: true,
-    draggable: true });
-  ec2.on('drag', on_drag);
-
-  // ec2 animations
-  scene.anims.create({
-    key: 'happy',
-    frames: [ { key: 'ec2', frame: 0} ],
-    frameRate: 20
-  });
-
-  scene.anims.create({
-    key: 'sad',
-    frames: [ { key: 'ec2', frame: 1} ],
-    frameRate: 20
-  });
-}
-
-function create_public_subnet(scene) {
-  bg = scene.add.image(0,0, 'pub_subnet');
-  nat_gateway_toggle = scene.add.image(275, 0, 'nat_gw');
-
-  // make toggle interactive
-  // where did 40 come from? Good question! I think it's the height/width of
-  // the toggle image?
-  nat_gateway_toggle.setInteractive({
-    hitArea: new Phaser.Geom.Circle(40, 40, 40),
-    hitAreaCallback: Phaser.Geom.Circle.Contains,
-    useHandCursor: true });
-    nat_gateway_toggle.on('pointerup', function() {
-      toggleNatGateway();
-  });
-
-  public_subnet = scene.add.container(610, 325, [bg, nat_gateway_toggle])
-  public_subnet.setSize(bg.width, bg.height);
-  public_subnet.setScale(0.8)
-
-  function toggleNatGateway() {
-    nat_gateway_enabled = !nat_gateway_enabled
-  }
+function toggleNatGateway() {
+  nat_gateway_enabled = !nat_gateway_enabled
 }
