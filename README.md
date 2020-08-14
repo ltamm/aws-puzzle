deaab96cce219f8a5652a6b49b634c09f1eb7431
Laura Tammpere <laura.tammpere@clio.com>
2020-08-13 20:43:07 -0700

    Added a private subnet

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Run with `rerun rackup`
+An experiment in interactive architecture diagrams as a tool for learning and understanding. 
+Also an excuse to try out [phaser.io](https://phaser.io/)
+
+Run with `rerun rackup` 
+The puzzle will be served to `localhost:4567`
--- public/scripts/canvas.js
+++ public/scripts/canvas.js
@@ -11,16 +11,17 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-var debugtext
-var ec2
 
 // Components
+var ec2
 var vpc
-var subnet
+var public_subnet
+var private_subnet
 
 function preload() {
   this.load.image('vpc', 'assets/vpc.png');
   this.load.image('pub_subnet', 'assets/pub_subnet.png');
+  this.load.image('priv_subnet', 'assets/priv_subnet.png');
   this.load.spritesheet('ec2', 
     'assets/ec2.png',
     { frameWidth: 300, frameHeight: 300 }
@@ -32,22 +33,30 @@ function create() {
   // Create VPC object
   vpc = this.add.sprite(500, 400, 'vpc');
   vpc.setInteractive({ pixelPerfect:true, draggable: true });
-  vpc.setScale(0.4);
+  vpc.setScale(.8);
   vpc.on('drag', function (pointer, dragX, dragY) {
     this.x = dragX;
     this.y = dragY;
   });
-
+  
   // Create public subnet object
-  subnet = this.add.sprite(600, 400, 'pub_subnet');
-  subnet.setInteractive({ pixelPerfect:true, draggable: true });
-  subnet.on('drag', function (pointer, dragX, dragY) {
+  subnet_public = this.add.sprite(600, 325, 'pub_subnet');
+  subnet_public.setInteractive({ pixelPerfect:true, draggable: true });
+  subnet_public.on('drag', function (pointer, dragX, dragY) {
+    this.x = dragX;
+    this.y = dragY;
+  });
+
+  // Create private subnet object
+  subnet_private = this.add.sprite(600, 475, 'priv_subnet');
+  subnet_private.setInteractive({ pixelPerfect:true, draggable: true });
+  subnet_private.on('drag', function (pointer, dragX, dragY) {
     this.x = dragX;
     this.y = dragY;
   });
 
   // Create ec2 object
-  ec2 = this.add.sprite(700, 400, 'ec2_sad');
+  ec2 = this.add.sprite(700, 325, 'ec2_sad');
   ec2.setInteractive({ pixelPerfect:true, draggable: true });
   ec2.setScale(0.3);
   ec2.on('drag', function (pointer, dragX, dragY) {
@@ -79,5 +88,5 @@ function update() {
 }
 
 function has_connectivity() {
- return subnet.getBounds().contains(ec2.x, ec2.y) && vpc.getBounds().contains(subnet.x, subnet.y);
+  return subnet_public.getBounds().contains(ec2.x, ec2.y) && vpc.getBounds().contains(subnet_public.x, subnet_public.y);
 }
